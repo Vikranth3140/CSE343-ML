a510e48bcc8a89897c05b5efeabcbecfacb7e6cf
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-09-12 22:59:36 +0530

    misc fix

--- A1/Q3/G.py
+++ A1/Q3/G.py
@@ -67,7 +67,7 @@ def perform_elastic_net(alpha, X_train, X_test, y_train, y_test):
     }
 
 # Try ElasticNet with different alpha values
-alpha_values = [0.1, 0.05, 0.001, 1.005, 1.0005]
+alpha_values = [0.1, 0.05, 0.001, 0.005, 0.0005]
 results = []
 
 for alpha in alpha_values:
